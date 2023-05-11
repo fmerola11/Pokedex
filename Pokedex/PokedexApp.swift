b84950d012696bd7e487bf11169ddb8a48bdce34
Francesco Merola <fmerola19@gmail.com>
2023-05-11 09:54:04 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Francesco Merola on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
